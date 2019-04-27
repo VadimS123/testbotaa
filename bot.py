d458f11fdde870079a6f8fecdf98b78e0e3de400
VadimS123 <49989982+VadimS123@users.noreply.github.com>
2019-04-27 19:34:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def ping(ctx):
   
 @Bot.command(pass_context= True)
 async def hellome(ctx):
-    await Bot.say("Привет {}".format(ctx.message.mention))
+    await Bot.say("Привет {}".format(ctx.message.author.mention))
 
 
 token = os.environ.get('BOT_TOKEN')
