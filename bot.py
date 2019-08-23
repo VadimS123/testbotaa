5163f73f03cde3509c8dc04ebb6c647a213c7daf
VadimS123 <49989982+VadimS123@users.noreply.github.com>
2019-08-23 23:07:48 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,14 +6,21 @@ Bot = commands.Bot(command_prefix='<')
 
 Bot.remove_command('help')
 
+
+
 @Bot.event
 async def on_ready():
     print("Now bot is online")
  
+
+
+
 @Bot.command(pass_context=True)
 async def test(ctx):
     await Bot.say("Работаю стабильно!")
  
+
+
 @Bot.command(pass_context= True)
 async def help(ctx):
     emb = discord.Embed(title= "Мои комманды:", colour= 0x39d0d6)
@@ -26,22 +33,45 @@ async def help(ctx):
     emb.add_field(name= "<avatar @user", value= "посмотреть аватар пользователя или свой аватар")
     emb.add_field(name= "<ban @user", value= "забанить пользователя")
     emb.add_field(name= "<kick @user", value= "кикнуть пользователя")
-    emb.add_field(name= "Скоро будет еще больше комманд!", value= "Обращайтесь за идеями к: Вадим#2677")
+    emb.add_field(name= "<dem", value= "Показать демку на сервере дискорд! (вы должны быть в звонке)")
+    emb.add_field(name= "Скоро будет еще больше комманд!", value= "Обращайтесь за идеями к: Hatsune Miku#2677")
     await Bot.say (embed= emb)
 
+
+
+
 @Bot.command(pass_context=True)
-async def botinfo(ctx):
-    await Bot.say("Автор бота: Вадим#2677")
-    await Bot.say("Официальная группа ВК: vk.com/truenobot_official")
+async def author(ctx):
+    await Bot.say("Автор бота: Hatsune Miku#2677")
  
+
+
 @Bot.command(pass_context=True)
+@commands.has_permissions(administrator=True)
 async def hello(ctx):
     await Bot.say("(@everyone) Привет всем!")
+
+
 
 @Bot.command(pass_context=True)
 async def ping(ctx):
     await Bot.say("Pong!")
   
+
+
+@Bot.command(pass_context= True)
+async def info(ctx, user: discord.User):
+    emb = discord.Embed(title= "Информация о пользователе {}".format(user.name),colour= 0x39d0d6)
+    emb.add_field(name= "Ник:", value= user.name)
+    emb.add_field(name= "ID:",value= user.id)
+    emb.set_thumbnail(url= user.avatar_url)
+    emb.set_author(name= Bot.user.name,url= "https://discordapp.com/oauth2/authorize?&client_id=570899950649606144&scope=bot&permissions=8")
+    emb.set_footer(text= "Запрошено: {}".format(user.name), icon_url= user.avatar_url)
+    await Bot.say(embed= emb)
+
+
+
+
 @Bot.command(pass_context= True)
 async def avatar(ctx, user: discord.User = None):
     if user == None:
@@ -54,17 +84,7 @@ async def avatar(ctx, user: discord.User = None):
         await Bot.say(embed= emb)
 
 
-@Bot.command(pass_context= True)
-async def info(ctx, user: discord.User):
-    emb = discord.Embed(title= "Информация о пользователе {}".format(user.name),colour= 0x39d0d6)
-    emb.add_field(name= "Ник:", value= user.name)
-    emb.add_field(name= "ID:",value= user.id)
-    emb.set_thumbnail(url= user.avatar_url)
-    emb.set_author(name= Bot.user.name,url= "https://discordapp.com/oauth2/authorize?&client_id=570899950649606144&scope=bot&permissions=8")
-    emb.set_footer(text= "Запрошено: {}".format(user.name), icon_url= user.avatar_url)
-    await Bot.say(embed= emb)
-    await Bot.delete_message(ctx.message)
- 
+
 @Bot.event
 async def on_message_delete(message):
     channel2 = message.channel
@@ -76,6 +96,8 @@ async def on_message_delete(message):
     emb.set_footer(text= "ID сообщения: **{}**  | Сегодня в {}".format(message.id, str(message.timestamp.strftime("%X"))))
     await Bot.send_message(channel, embed = emb)
 
+
+
 @Bot.command(pass_context= True)
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, user: discord.User, reason= None):
@@ -83,6 +105,20 @@ async def ban(ctx, user: discord.User, reason= None):
     await Bot.delete_message(ctx.message)
     emb = discord.Embed(title= "**Участник {}, был забанен.**".format(user), colour= 0xb0fe0a)
     await Bot.say(embed= emb)
+
+
+
+@Bot.command(brief = 'Сделать звонок на сервере')
+async def dem(ctx):
+
+    Guild = ctx.message.guild.id
+    channel = ctx.message.author.voice.channel.id
+
+    embed = discord.Embed(colour = 0xff000, description = f'https://discordapp.com/channels/{Guild}/{channel}')
+
+    await ctx.send(embed = embed)
+
+
 
 @Bot.command(pass_context= True)
 @commands.has_permissions(kick_members=True)
@@ -92,15 +128,12 @@ async def kick(ctx, user: discord.User, reason= None):
     emb = discord.Embed(title= "**Участник __{}__, был кикнут.**".format(user), colour= 0x3600ff)
     await Bot.say(embed= emb)
 
+
+
 @Bot.command(pass_context= True)
 @commands.has_permissions(administrator= True)
 async def say(ctx):
     await Bot.say(ctx.message.content[4:])
-    await Bot.delete_message(ctx.message)
-
-@Bot.command(pass_context= True)
-async def asay(ctx):
-    await Bot.say(ctx.message.content[5:])
     await Bot.delete_message(ctx.message)
 
 token = os.environ.get('BOT_TOKEN')
