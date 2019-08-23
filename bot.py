6a4a6840b77f399d74ed4286a4e2c2700f1e8e6a
VadimS123 <49989982+VadimS123@users.noreply.github.com>
2019-08-24 00:18:25 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,13 +97,15 @@ async def on_message_delete(message):
     emb.set_footer(text= "ID сообщения: **{}**  | Сегодня в {}".format(message.id, str(message.timestamp.strftime("%X"))))
     await Bot.send_message(channel, embed = emb)
 
-async def dem(ctx):
-    Server = ctx.message.server.id
+@Bot.command(brief = 'Сделать звонок на сервере')
+async def call(ctx):
+
+    Guild = ctx.message.guild.id
     channel = ctx.message.author.voice.channel.id
 
-    embed = discord.Embed(colour = 0xff000, description = f'https://discordapp.com/channels/{Server}/{channel}')
-    
-    await Bot.say(embed= embed)
+    embed = discord.Embed(colour = 0xff000, description = f'https://discordapp.com/channels/{Guild}/{channel}')
+
+    await ctx.send(embed = embed)
 
 @Bot.command(pass_context= True)
 @commands.has_permissions(ban_members=True)
